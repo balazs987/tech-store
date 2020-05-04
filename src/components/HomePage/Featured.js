c002bb4b2f7b164ab17e1d23f19267f8a63e16eb
balazs987 <bpataki1984@gmail.com>
2020-05-04 14:19:37 +0200

    featured product

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Product from "../Product";
+import Title from "../Title";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../../context";
 
 function Featured() {
-  return <div>hello from featured products</div>;
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="featured products" center="true" />
+        <div className="row">
+          <ProductConsumer>
+            {(value) => {
+              const { featuredProducts } = value;
+              return featuredProducts.map((product) => (
+                <Product key={product.id} product={product} />
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Featured;
