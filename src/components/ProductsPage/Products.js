ffb248b6652086cd80b3393b63b626f6228b08f2
balazs987 <bpataki1984@gmail.com>
2020-05-11 15:41:31 +0200

    product filter

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { ProductConsumer } from "../../context";
-import Title from "../Title";
-import Product from "../Product";
+import React from 'react';
+import { ProductConsumer } from '../../context';
+import Title from '../Title';
+import Product from '../Product';
+import ProductFilter from './ProductFilter';
 
 function Products() {
   return (
@@ -12,10 +13,24 @@ function Products() {
           <section className="py-5">
             <div className="container">
               <Title center title="our products" />
+              <ProductFilter />
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <h6 className="text-title">
+                    total products: {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
               <div className="row py-5">
-                {filteredProducts.map((product) => (
-                  <Product key={product.id} product={product} />
-                ))}
+                {filteredProducts.length === 0 ? (
+                  <div className="col text-title text-center">
+                    sorry, no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map((product) => (
+                    <Product key={product.id} product={product} />
+                  ))
+                )}
               </div>
             </div>
           </section>
--- src/context/context.js
+++ src/context/context.js
@@ -23,6 +23,12 @@ class ProductProvider extends Component {
     featuredProducts: [],
     singleProduct: {},
     loading: true,
+    search: '',
+    price: 0,
+    min: 0,
+    max: 0,
+    company: 'all',
+    shipping: false,
   };
 
   componentDidMount() {
@@ -44,6 +50,9 @@ class ProductProvider extends Component {
       (item) => item.featured === true
     );
 
+    // get max price
+    let maxPrice = Math.max(...storeProducts.map((item) => item.price));
+
     this.setState(
       {
         storeProducts,
@@ -52,6 +61,8 @@ class ProductProvider extends Component {
         cart: this.getStorageCart(),
         singleProduct: this.getStorageProduct(),
         loading: false,
+        price: maxPrice,
+        max: maxPrice,
       },
       () => this.addTotals()
     );
@@ -240,6 +251,48 @@ class ProductProvider extends Component {
     );
   };
 
+  // handle filtering
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value =
+      event.target.type === 'checkbox'
+        ? event.target.checked
+        : event.target.value;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.sortData
+    );
+  };
+
+  sortData = () => {
+    const { storeProducts, price, company, shipping, search } = this.state;
+    let tempProducts = [...storeProducts];
+    let tempPrice = parseInt(price);
+    // price range
+    tempProducts = tempProducts.filter((item) => item.price <= tempPrice);
+    // company filtering
+    if (company !== 'all') {
+      tempProducts = tempProducts.filter((item) => item.company === company);
+    }
+    // shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter((item) => item.freeShipping === true);
+    }
+    // search
+    if (search.length > 0) {
+      tempProducts = tempProducts.filter((item) => {
+        let tempSearch = search.toLowerCase();
+        let tempTitle = item.title.toLowerCase().slice(0, search.length);
+        if (tempSearch === tempTitle) {
+          return item;
+        }
+      });
+    }
+    this.setState({ filteredProducts: tempProducts });
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -255,6 +308,7 @@ class ProductProvider extends Component {
           decrement: this.decrement,
           removeItem: this.removeItem,
           clearCart: this.clearCart,
+          handleChange: this.handleChange,
         }}
       >
         {this.props.children}
